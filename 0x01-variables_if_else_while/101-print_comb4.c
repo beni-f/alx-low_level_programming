d94f302b15e09d0273a0ff153fc2e9092b2f071c
beni-f <benifissha@gmail.com>
2023-06-17 02:14:30 +0300

    Print different combinations of numbers from three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - prints combinations of numbers from three digits
+ * Return: returns 0
+*/
 int main(void)
 {
 	int digit1, digit2, digit3;
-	
+
 	for (digit1 = '0'; digit1 <= '9'; digit1++)
 	{
 		for (digit2 = '0'; digit2 <= '9'; digit2++)
@@ -17,21 +20,21 @@ int main(void)
 					putchar(digit2);
 
 					putchar(digit3);
-					
+
 					if (digit1 != '7')
 					{
 						putchar(',');
 
 						putchar(' ');
 					}
-					
+
 				}
 			}
 		}
 	}
-	
-	putchar('\n');	
-	
+
+	putchar('\n');
+
 	return (0);
-			               			
+
 }
