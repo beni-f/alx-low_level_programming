47d071ac607dcb24d115565ca8e6cd0ad549e518
beni-f <benifissha@gmail.com>
2023-06-16 13:00:46 +0300

    Print numbers with commas

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
-
+/**
+ * main - this function returns numbers from 0-9 with commas
+ * Return: returns 0
+*/
 int main(void)
 {
 	int num;
-	
+
 	for (num = '0'; num <= '9'; num++)
 	{
 		putchar(num);
-		
+
 		if (num != '9')
 		{
 			putchar(',');
-			
+
 			putchar (' ');
 		}
 	}
-	
+
 	putchar('\n');
 
 	return (0);
