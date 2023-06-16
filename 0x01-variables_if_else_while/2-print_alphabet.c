cecad27ee9d303482ade3ab74674b61d23350e4f
beni-f <benifissha@gmail.com>
2023-06-16 09:17:34 +0300

    Print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints out all lower case alphabets
+ * Return: returns 0
+*/
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
