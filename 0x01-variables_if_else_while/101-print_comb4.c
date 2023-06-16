5342d659382cddfb617f1c0826f67c2bd5bf8ab3
beni-f <benifissha@gmail.com>
2023-06-17 02:03:40 +0300

    Diffrent combinations of numbers from 3 digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int digit1, digit2, digit3;
+	
+	for (digit1 = '0'; digit1 <= '9'; digit1++)
+	{
+		for (digit2 = '0'; digit2 <= '9'; digit2++)
+		{
+			for (digit3 = '0'; digit3 <= '9'; digit3++)
+			{
+				if (digit1 < digit2 && digit2 < digit3)
+				{
+					putchar(digit1);
+
+					putchar(digit2);
+
+					putchar(digit3);
+					
+					if (digit1 != '7')
+					{
+						putchar(',');
+					}
+					putchar(' ');
+				}
+			}
+		}
+	}
+	
+	putchar('\n');	
+	
+	return (0);
+			               			
+}
