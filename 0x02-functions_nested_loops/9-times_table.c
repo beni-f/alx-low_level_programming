a855233da78d8351214bf6c2d5fcd65af4570d48
beni-f <benifissha@gmail.com>
2023-06-20 22:22:29 +0300

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,18 +16,12 @@ void times_table(void)
 			product = a * b;
 			if (b > 0)
 			{
-				_putchar(',');
+				printf(", ");
 
-				_putchar(' ');
 			}
-			if (product < 10)
-				_putchar(product + '0');
-			else
-			{
-				_putchar(product / 10 + '0');
 
-				_putchar(product % 10 + '0');
-			}
+		printf("%i", product);
+
 		}
 		putchar('\n');
 	}
