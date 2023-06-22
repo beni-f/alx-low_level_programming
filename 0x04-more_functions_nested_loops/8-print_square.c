edaf2904196a5cbf7f88e8dbe6cd5be04c9a213b
beni-f <benifissha@gmail.com>
2023-06-22 22:35:04 +0300

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,8 +8,13 @@
 void print_square(int size)
 {
 	int a, b;
+	
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
 
-	for (a = 0; a < size; a++)
+	for (a = 1; a < size; a++)
 	{
 		for (b = 0; b < size; b++)
 		{
