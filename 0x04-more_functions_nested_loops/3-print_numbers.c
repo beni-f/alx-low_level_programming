9ade1dcc9dc7d6a49a8f709b76b08e684bc643d7
beni-f <benifissha@gmail.com>
2023-06-22 18:32:24 +0300

    print numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+*/
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		printf("%i", a);
+	}
+}
