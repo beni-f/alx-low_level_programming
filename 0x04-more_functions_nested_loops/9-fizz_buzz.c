72f03bc83fec787661cc736fbf349633ecb840c4
beni-f <benifissha@gmail.com>
2023-06-22 22:54:55 +0300

    Fizz bizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
-
+/**
+ * main - prints fizz buzz
+ *
+ * Return: returns nothing
+*/
 int main(void)
 {
 	int a;
+
 	for (a = 1; a <= 100; a++)
 	{
 		if (a % 3 == 0 && a % 5 == 0)
-		{	
+		{
 			printf("FizzBuzz ");
 		}
-		
+
 		else if (a % 5 == 0)
 		{
 			printf("Buzz ");
 		}
-		
+
 		else if (a % 3 == 0)
 		{
 			printf("Fizz ");
