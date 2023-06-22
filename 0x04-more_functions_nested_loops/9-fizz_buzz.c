1282b095662ac53b9126edecd93bf70dca2bcb20
beni-f <benifissha@gmail.com>
2023-06-22 22:50:32 +0300

    Fizz bizz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a;
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 3 == 0 && a % 5 == 0)
+		{	
+			printf("FizzBuzz ");
+		}
+		
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		
+		else if (a % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%i ", a);
+		}
+	}
+
+	printf("\n");
+}
