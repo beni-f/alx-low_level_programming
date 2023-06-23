d7f15b5c36ae8b81883dfea341d0acb100cf5c5b
beni-f <benifissha@gmail.com>
2023-06-23 11:42:49 +0300

    print 0 t0 14, 10 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,13 +13,9 @@ void more_numbers(void)
 		for (b = 0; b <= 14; b++)
 		{
 			if (b >= 10)
-			{
 				_putchar(b / 10 + '0');
 
-				_putchar(b % 10 + '0');
-			}
-			else
-				_putchar(b + '0');
+			_putchar(b % 10 + '0');
 		}
 
 		_putchar('\n');
