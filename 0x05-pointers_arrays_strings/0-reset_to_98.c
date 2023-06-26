52110e9eb78ee806c78c14d48232aae0326c1223
beni-f <benifissha@gmail.com>
2023-06-26 18:32:53 +0300

    reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 #include "main.h"
 /**
  * reset_to_98 - updates the value of a pointer to 98
- * @*n - pointer to integer parameter
+ * @n: pointer to integer parameter
 */
 void reset_to_98(int *n)
 {
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+
+void print_array(int *a, int n)
+{
+	for (a = 0; a < n; a++)
+	{
+		printf("%i, ", a[n]);
+	}
+}		
