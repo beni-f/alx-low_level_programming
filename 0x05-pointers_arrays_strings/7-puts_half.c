7299d6a2ef56d8c6aa7d677bf4c77ccb8d4bdd38
beni-f <benifissha@gmail.com>
2023-06-26 15:23:27 +0300

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+void puts_half(char *str)
+{
+	int index, i;
+
+	for (index = 0; str[index] != '\0'; index++)
+		;
+	if (index % 2 != 0)
+	{
+		for (i = (index - 1) / 2; i <= index; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = index / 2; i <= index; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
