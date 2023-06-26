89b42f175c109b511e8a47af726c702cf52884ba
beni-f <benifissha@gmail.com>
2023-06-26 14:55:04 +0300

    puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts2 - prints every other string
+ * @str: pointer to character parameter
+*/
+void puts2(char *str)
+{
+	int index, i;
+
+	for (index = 0; str[index] != '\0'; index++)
+		;
+
+	for (i = 0; i <= index; i++)
+	{
+		if (i == 0 || i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
