e106f5ba35eb0bf40ef8ec2cdbb73cd4c5094053
beni-f <benifissha@gmail.com>
2023-06-26 18:42:56 +0300

    print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * print_array - prints array
+ * @a: pointer to integer parameter
+ * @n : integer parameter
+*/
 void print_array(int *a, int n)
 {
-	for (a = 0; a < n; a++)
+	int i;
+
+	for (i = 0; i < n; i++)
 	{
-		printf("%i, ", a[n]);
+		printf("%i", a[i]);
+
+		if (i != n - 1)
+			printf(", ");
 	}
-}		
+	printf("\n");
+}
