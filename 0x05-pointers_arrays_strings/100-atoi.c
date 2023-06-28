8e979829a8aef0ee201d4ffaba070692e3ed41fd
beni-f <benifissha@gmail.com>
2023-06-28 15:34:45 +0300

    convert string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+int _atoi(char *s)
+{
+	unsigned int i = 0, length = 0, num = 0, sign = 1;
+
+	for (length = 0; s[length] != '\0'; length++)
+		;
+	for (i = 0; i < length; i++)
+	{
+		if (s[i] == '-')
+			sign = sign * -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = (num * 10) + (s[i] - '0');
+		}
+	}
+	return (num * sign);
+}
