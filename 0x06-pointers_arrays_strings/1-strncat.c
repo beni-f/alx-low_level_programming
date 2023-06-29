c7142fb710c700b76f3087f52b7dfb109e345105
beni-f <benifissha@gmail.com>
2023-06-29 20:32:11 +0300

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,4 +3,18 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
+	int i = 0;	
+	
+	while (*dest)
+		dest++;
+	
+	while (*src && i < n)
+	{
+		*dest++ = *src++;
+	
+		i++;
+	}
+	return dest;
+	
+}
 	
