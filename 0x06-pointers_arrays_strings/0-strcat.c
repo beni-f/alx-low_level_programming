2d9f3cfe3bfd25a3e17d4f6b1358afaa99a36a23
beni-f <benifissha@gmail.com>
2023-06-29 17:29:37 +0300

    string append

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+ * _strcat - returns a string that is appended
+ *
+ * @dest: string that will be appended in this function
+ * @src: string that appends its content to dest
+ *
+ * Return: returns string
+*/
 char *_strcat(char *dest, char *src)
 {
 	int length1, length2, i;
@@ -8,10 +15,10 @@ char *_strcat(char *dest, char *src)
 	for (length1 = 0; *src < '\0'; length1++)
 		;
 	for (length2 = 0; *dest < '\0'; length2++)
-		;	
+		;
 	for (i = 0; i < length1; i++)
 	{
-		dest[length2 + i] = src[i]; 
+		dest[length2 + i] = src[i];
 	}
-	return dest;
+	return (dest);
 }
