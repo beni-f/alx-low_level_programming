c774afb45d53a16487c6152106332b8fbe313527
beni-f <benifissha@gmail.com>
2023-06-30 16:08:08 +0300

    reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,11 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * reverse_array - takes an array and reverses it
+ * 
+ * @n: pointer to integer parameter
+ * @a: integer parameter
+*/
 void reverse_array(int *a, int n)
 {
 	int x, y, i;
