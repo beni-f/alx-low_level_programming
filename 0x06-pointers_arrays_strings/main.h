d01b5109a4f4ccf0f4766a3b9dff3425f515b5bd
beni-f <benifissha@gmail.com>
2023-07-02 18:54:03 +0300

    print buffer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+void print_buffer(char *b, int size);
 char *infinite_add(cahr *n1, char *n2, char *r, int size_r);
 void print_number(int n);
 char *rot13(char *str);
