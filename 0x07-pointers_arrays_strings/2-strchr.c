663e00a840e272eb6ff598cdaa30f3bf540caa0b
beni-f <benifissha@gmail.com>
2023-07-03 15:19:24 +0300

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,21 +1,28 @@
 #include "main.h"
 #include <stdio.h>
-
+/**
+ * _strchr - locates a character in a string
+ *
+ * @s: string to be encoded
+ * @c : character parameter
+ *
+ * Return: returns string
+*/
 char *_strchr(char *s, char c)
 {
 	int i, length = 0;
 
 	for (length = 0; s[length] != '\0'; length++)
 		;
-	
+
 	for (i = 0; i < length; i++)
 	{
 		if (s[i] == c)
 		{
-			
+
 			return (&s[i]);
 		}
 	}
 	return (0);
 }
-		
+
