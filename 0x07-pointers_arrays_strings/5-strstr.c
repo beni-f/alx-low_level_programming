5a0daee54077117096aa6b364f947e4c52f4e522
beni-f <benifissha@gmail.com>
2023-07-04 12:45:00 +0300

    chessboard

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,13 +10,20 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	int i, j, length, counter = 0;
+	
+	for (length = 0; needle[length] != '\0'; length++)
 
 	for (i = 0; needle[i] != '\0'; i++)
 	{
 		for (j = 0; haystack[j] != '\0'; j++)
 		{
 			if (needle[i] == haystack[j])
+			{
+				counter++;
+				break;
+			}
+			if (length == counter)
 				return (&haystack[j]);
 		}
 	}
