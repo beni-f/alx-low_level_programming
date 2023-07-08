2bae402fcaae9aa74b237455022009096b3e2924
beni-f <benifissha@gmail.com>
2023-07-08 14:56:30 +0300

    multiply two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,2 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers
+ *
+ * @argv: a pointer to a character array
+ * @argc: length of the command line argument
+ *
+ * Return: returns 0
+*/
+int main(int argc, char *argv[])
+{
+	int i, product;
 
+	if (argc == 1)
+	{
+		printf("Error\n");
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		product = atoi(argv[i]) * atoi(argv[i + 1]);
+		printf("%d\n", product);
+		break;
+	}
+
+	return (0);
+}
