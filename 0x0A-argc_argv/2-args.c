b9876bff31e09cffe1fb8ffd6fd29af1df4a637a
beni-f <benifissha@gmail.com>
2023-07-08 14:32:49 +0300

    print argument

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints every arguments in a new line
+ *
+ * @argv: pointer to a character array
+ * @argc: length of the command line argument
+ *
+ * Return: returns 0
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	
+	return (0);
+}
