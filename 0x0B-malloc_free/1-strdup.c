b2d56ba3be33d17b0768770f215c1a336f8ae5b6
beni-f <benifissha@gmail.com>
2023-07-10 18:08:15 +0300

    copy string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - MAIN entry
+ *
+ * @str: input
+ *
+ * Return: returns string
+*/
+char *_strdup(char *str)
+{
+	int i, length;
+
+	char *a;
+
+	for (length = 0; str[length] != '\0'; length++)
+		;
+
+	a = malloc(sizeof(*str));
+
+	if (length == 0)
+		return (NULL);
+
+	for (i = 0; i < length; i++)
+		a[i] = str[i];
+
+	return (a);
+}
+
+
