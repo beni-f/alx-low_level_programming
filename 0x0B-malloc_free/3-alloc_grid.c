db1ac9beeb054b0467f948cb83aa5812e844690e
beni-f <benifissha@gmail.com>
2023-07-10 22:54:26 +0300

    allocate 2d grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,14 +20,20 @@ int **alloc_grid(int width, int height)
 
 	if (a == NULL)
 	{
-		return (NULL);
 		free(a);
+		return (NULL);
 	}
 
 	for (i = 0; i < height; i++)
 	{
 		a[i] = malloc(sizeof(int) * width);
-
+	
+		if (a[i] == NULL)
+		{
+			free(a);
+			return (NULL);
+		}
+	
 		for (j = 0; j < width; j++)
 		{
 			a[i][j] = 0;
