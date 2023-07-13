b2643afdf417678f44be8eaa046bf57735e19365
beni-f <benifissha@gmail.com>
2023-07-13 13:23:53 +0300

    concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (len2 = 0; s2[len2] != '\0'; len2++)
 		;
 
-	a = malloc(len1 + len2 + 1);
+	a = malloc(sizeof(char) * (len1 + len2 + 1));
 
 	if (a == NULL)
 		return (NULL);
