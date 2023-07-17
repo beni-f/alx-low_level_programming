c718e57ba4937e65ecbe39a9eb7b5aad0e9afb35
beni-f <benifissha@gmail.com>
2023-07-17 13:00:02 +0300

    store dog info

---
+++ dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct to store information about a dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Define new type struct dog to store information
+ */
+struct dog
+{
+	char *name;
+
+	float age;
+
+	char *owner;
+}
