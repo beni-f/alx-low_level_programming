fb29a2b7bdde97b1f92beccb5cc1ac60f3943de9
beni-f <benifissha@gmail.com>
2023-07-17 13:38:59 +0300

    readme

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
 /**
  * init_dog - initializes a variable of type struct dog
@@ -16,6 +15,4 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-
-	return (d);
 }
