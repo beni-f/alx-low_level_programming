7382f2c2662acac5774fe4332e63ca8e37c6de9a
beni-f <benifissha@gmail.com>
2023-07-19 14:05:39 +0300

    find an integer

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - finds an integer
+ *
+ * @array: pointer to an integer
+ * @size: integer
+ * @cmp: pointer to a function
+ *
+ * Return: Always 0
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, s;
+
+	if (array == NULL || cmp == NULL)
+		return (-1);
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if(cmp(array[i]))
+		{
+			s = i;
+			break;
+		}
+	}
+	return (s);
+}
