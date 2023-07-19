b52aac69a0e5126c108d764f8f20828f7e5bd9a3
beni-f <benifissha@gmail.com>
2023-07-19 14:45:43 +0300

    find an integer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,9 +13,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 	
-	if (array == NULL || size <= 0)
-		return (NULL);
-
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
