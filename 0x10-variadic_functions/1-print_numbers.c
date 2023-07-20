f6a9e89e11c888cc023251d5578652f50e790e13
beni-f <benifissha@gmail.com>
2023-07-20 20:20:59 +0300

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -24,7 +24,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 		printf("%i", x);
 
-		if (i != n - 1)
+		if (i != n - 1 && separator != NULL)
 			printf("%s", separator);
 	}
 
