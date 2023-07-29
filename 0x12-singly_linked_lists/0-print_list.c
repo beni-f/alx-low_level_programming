f80a6e522743328e09456a8d56951a6b8a246608
beni-f <benifissha@gmail.com>
2023-07-29 15:35:09 +0300

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include "lists.h"
-
+/**
+ * print_list - prints elements in a linked list
+ * @h: pointer to a structure
+ *
+ * Return: returns the number of elements
+*/
 size_t print_list(const list_t *h)
-{	
+{
 	size_t n = 0;
 
 	while (h != 0)
@@ -16,4 +21,4 @@ size_t print_list(const list_t *h)
 	}
 	return (n);
 }
-	
+
