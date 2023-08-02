c985c3d1117b53c3d7abc6a34b8219a8236bdd59
beni-f <benifissha@gmail.com>
2023-08-02 22:38:55 +0300

    free memory

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,6 +1,11 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * free_listint - frees memory
+ *
+ * @head: pointer to a sturcture
+*/
 void free_listint(listint_t *head)
 {
 	listint_t *temp = head;
@@ -8,6 +13,6 @@ void free_listint(listint_t *head)
 	while ((temp = head) != 0)
 	{
 		head = head->next;
-		free(head);
+		free(temp);
 	}
 }
