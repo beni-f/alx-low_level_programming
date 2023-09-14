c6495227423fa956595c95b2c7dbdc4e05af3be9
beni-f <benifissha@gmail.com>
2023-09-14 18:09:05 +0300

    updated

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -10,6 +10,7 @@ typedef struct listint_t{
     struct listint_t *prev;
 }dlistint_t;
 size_t print_dlistint(const dlistint_t *h);
+void free_dlistint(dlistint_t *head);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
