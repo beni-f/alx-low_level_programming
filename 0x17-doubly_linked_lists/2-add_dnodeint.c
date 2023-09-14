0174f4ecfaab3455f40c492757538fd1d709ad67
beni-f <benifissha@gmail.com>
2023-09-14 16:44:01 +0300

    updated

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
     dlistint_t *newnode;
     if (!*head)
     {
-        return(0);
+        return (0);
     }
     newnode = (dlistint_t *) malloc(sizeof(dlistint_t));
     if (newnode == NULL)
@@ -26,5 +26,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
         (*head)->prev = newnode;
     }
     *head = newnode;
+
     return (newnode);
 }
