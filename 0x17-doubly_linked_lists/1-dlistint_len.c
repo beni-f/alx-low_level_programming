98abab874436f669a804a59c6a72f9d2c90e7d6b
beni-f <benifissha@gmail.com>
2023-09-14 15:53:31 +0300

    updated

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,10 @@
 #include "lists.h"
-
+/**
+ * dlistint_len - prints a doubly linked list
+ * @h: pointer to the list
+ *
+ * Return: number of nodes in the list
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
     const dlistint_t *temp = h;
@@ -18,28 +23,4 @@ size_t dlistint_len(const dlistint_t *h)
         return (count);
     }
 
-}
-int main(void)
-{
-    dlistint_t *head;
-    dlistint_t *new;
-    dlistint_t hello = {8, NULL, NULL};
-    size_t n;
-
-    head = &hello;
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-    {
-        dprintf(2, "Error: Can't malloc\n");
-        return (EXIT_FAILURE);
-    }
-    new->n = 9;
-    head->prev = new;
-    new->next = head;
-    new->prev = NULL;
-    head = new;
-    n = dlistint_len(head);
-    printf("-> %lu elements\n", n);
-    free(new);
-    return (EXIT_SUCCESS);
 }
