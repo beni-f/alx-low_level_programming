b7affae7d9e67f6dab2bf09919ae58e741b81d72
beni-f <benifissha@gmail.com>
2023-09-14 18:00:44 +0300

    updated

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -29,20 +29,3 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
     return (newnode);
 }
-
-int main(void)
-{
-    dlistint_t *head;
-
-    head = NULL;
-    add_dnodeint(&head, 0);
-    add_dnodeint(&head, 1);
-    add_dnodeint(&head, 2);
-    add_dnodeint(&head, 3);
-    add_dnodeint(&head, 4);
-    add_dnodeint(&head, 98);
-    add_dnodeint(&head, 402);
-    add_dnodeint(&head, 1024);
-    print_dlistint(head);
-    return (EXIT_SUCCESS);
-}
