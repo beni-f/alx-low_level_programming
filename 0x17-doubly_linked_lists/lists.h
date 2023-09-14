a48a0b9148d6b912d03e648ab5d9a2bf3689ded9
beni-f <benifissha@gmail.com>
2023-09-14 15:43:05 +0300

    inital commit

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 typedef struct listint_t{
@@ -9,4 +10,5 @@ typedef struct listint_t{
     struct listint_t *prev;
 }dlistint_t;
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 #endif
