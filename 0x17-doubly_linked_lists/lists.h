89891f1675d11b64356b034a32cd078299081e6f
beni-f <benifissha@gmail.com>
2023-09-15 11:52:53 +0300

    updated

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -4,11 +4,21 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-typedef struct listint_t{
-    size_t n;
-    struct listint_t *next;
-    struct listint_t *prev;
-}dlistint_t;
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * 
+ */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int sum_dlistint(dlistint_t *head);
