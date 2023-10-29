a13a6d91f686a6745ed4d45901ae8138d07d272d
beni-f <benifissha@gmail.com>
2023-10-29 22:22:03 +0300

    initial commit

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -44,4 +44,5 @@ hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const char *str);
 unsigned long int key_index(const char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
 #endif
