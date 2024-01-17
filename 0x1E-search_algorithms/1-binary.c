c779aec8a8117c86d95482bbb5d3b9ee801187fb
beni-f <benifissha@gmail.com>
2024-01-17 12:13:59 +0300

    update

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -12,6 +12,9 @@ int binary_search(int *array, size_t size, int value)
     int l, r, mid, i;
     l = 0;
     r = size - 1;
+    
+    if (array == NULL)
+        return (-1);
     while(l <= r)
     {
         mid = (l + r)/2;
@@ -32,4 +35,17 @@ int binary_search(int *array, size_t size, int value)
             l = mid + 1;
     }
     return -1;
+}
+
+int main(void)
+{
+    int array[] = {
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+    };
+    size_t size = sizeof(array) / sizeof(array[0]);
+
+    printf("Found %d at index: %d\n\n", 2, binary_search(array, size, 2));
+    printf("Found %d at index: %d\n\n", 5, binary_search(array, 5, 5));
+    printf("Found %d at index: %d\n", 999, binary_search(array, size, 999));
+    return (EXIT_SUCCESS);
 }
